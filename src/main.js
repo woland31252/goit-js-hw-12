d0644910339cfcb22a0c2e23c56fcd2b50336021
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-03-02 21:43:00 +0200

    fix load-more

--- src/main.js
+++ src/main.js
@@ -158,39 +158,6 @@ function appendToList(markup) {
   lightbox.refresh();
 }
 
-// function createMarkup({
-//   webformatURL,
-//   largeImageURL,
-//   tags,
-//   likes,
-//   views,
-//   comments,
-//   downloads,
-// }) {
-//   return `<div class="photo-card">
-//   <a class = "gallery__link" href="${largeImageURL}">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//       ${likes}
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//       ${views}
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//       ${comments}
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//       ${downloads}
-//     </p>
-//   </div>
-//   </a>
-// </div>`;
-// }
 
 function clearList() {
   gallery.innerHTML = '';
