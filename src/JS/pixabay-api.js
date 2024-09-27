f51c75b6c23d6f6ed95db45dfc2e720b65fef11c
Volodymyr <woland31252@gmail.com>
2024-09-27 19:46:03 +0300

    add backgrond and header

--- src/JS/pixabay-api.js
+++ src/JS/pixabay-api.js
@@ -4,17 +4,6 @@ const KEY = '33796051-1e422d07d8c9fc912bc2a7eb4';
 const param =
   'per_page=15&image_type=photo&orientation=horyzontal&safesearch=true';
 
-
-// export default function ImageApiService(search) {
-//   const URL = `${ENDPOINT}?key=${KEY}&q=${search}&${param}`;
-//   return fetch(URL).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
 export default class ImageApiService {
   constructor() {
     this.page = 1;
